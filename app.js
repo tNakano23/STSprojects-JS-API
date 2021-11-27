940421fcb77e3b33a01eeb3e796ab0509bca7b2c
tNakano23 <89465605+tNakano23@users.noreply.github.com>
2021-11-28 02:23:54 +0900

    com4

--- app.js
+++ app.js
@@ -1,50 +1,46 @@
 $(function () {
     $("#serch_btn").click(function () {
         // 入力された値を取得
-        $input_postcode = $('#zipcode').val();
+        var input_postcode = $('#zipcode').val();
         // urlを設定
-        var url = "https://zipcloud.ibsnet.co.jp/api/search" 
+        var url = "https://zipcloud.ibsnet.co.jp/api/search";
         // 送るデータを成形する
-        var param = { zipcode: $input_postcode };
-        $('#prefcode').html("001");
+        var param = { zipcode: input_postcode };
 
         // サーバーと通信(Ajax)
-        
         $.ajax({
             type: "GET", 
             cache: false,
             data: param,
-            // data: { zipcode: $input_postcode },
             url: url,
             dataType: "jsonp"
         })
-        
         .done(function (res) {
+            console.log(res.results);
+
             if (res.status != 200) {
                 // 通信には成功。APIの結果がエラー
                 // エラー内容を表示
-                $('#zip_result').html(res.message);
+                $('#zip_result').html("<p>入力値が適切でない、もしくは入力値の郵便番号が存在しません。</p>");
 
             } else {
+                var result = res.results[0]
+                console.log(result);
                 //住所を表示
-                $('#prefcode').val(res.prefcode);
-                $('#address1').val(res.address1);
-                $('#address2').val(res.address2);
-                $('#address3').val(res.address3);
-                $('#kana1').val(res.kana1);
-                $('#kana2').val(res.kana2);
-                $('#kana3').val(res.kana3);
+                $('#prefcode').html(result.prefcode);
+                $('#address1').html(result.address1);
+                $('#address2').html(result.address2);
+                $('#address3').html(result.address3);
+                $('#kana1').html(result.kana1);
+                $('#kana2').html(result.kana2);
+                $('#kana3').html(result.kana3);
             }
 
         })
-        
         .fail(function (error) {
             console.log(error);
             $('#zip_result').html("<p>通信エラーです。時間をおいてお試しください</p>");
         });
-        $('#address1').html("002");
-        $('#address2').html(res);
-        console.log(res);
     });
 });
 
